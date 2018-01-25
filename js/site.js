ee8826b5aff6d48d2218e4ce78ebd9b5bb396458
Michael Hartl <michael@michaelhartl.com>
2018-01-24 19:34:17 -0800

    Get gallery JS working

--- js/site.js
+++ js/site.js
@@ -1,40 +1,82 @@
-$(document).ready(function() {
+function swapImage(clickedImage, displayImage) {
+  newImageSrc = clickedImage.dataset.largeVersion;
+  displayImage.setAttribute("src", newImageSrc);
+}
 
-  $('#gallery-thumbs a').click(function(event) {
-    //don't follow link
-    event.preventDefault();
-    //get path to new image
-    var imgPath = $(this).attr('href');
-    //get reference to old image
-    var oldImage = $('#gallery-photo img');
+function currentThumbDiv() {
+  return document.querySelector(".current");
+}
 
-    //create HTML for new image
-    var newImage = $('<img src="' + imgPath +'">');
-    //make new image invisible
-    newImage.hide();
-    //add to the #photo div
-    $('#gallery-photo').prepend(newImage);
-    //fade in new image
-    var fade_time = 1000;
+function currentThumb() {
+  return currentThumbDiv().querySelector("img");
+}
 
-    $('#gallery-info h3').html($(this).find('.photo-title').html() || 'No Title');
-    $('#gallery-info p').html($(this).find('.photo-desc').html() || 'No Description');
+function setGalleryInfo() {
+  let infoDiv = document.getElementById("gallery-info");
+  let infoContent = `<h3>${currentThumb().dataset.title}</h3>` +
+                    `<p>${currentThumb().dataset.description}</p>`
+  infoDiv.innerHTML = infoContent;
+}
 
-    if(!$(this).hasClass('current')) {
-      $('.current').removeClass('current')
-      $(this).addClass('current');
-    }
+function setAsCurrent(image) {
+  currentThumbDiv().classList.remove("current");
+  image.parentNode.classList.add("current");
+}
 
-     //fade out old image and remove from DOM
-     oldImage.fadeOut(fade_time, function(){
-       $(this).remove();
-      });
+document.addEventListener("DOMContentLoaded", function() {
+  let gallery      = document.getElementById("gallery");
+  let displayDiv   = document.getElementById("gallery-photo");
+  let displayImage = displayDiv.querySelector("img");
+  let images       = gallery.querySelectorAll("img");
 
-     newImage.fadeIn(fade_time);
-  }); // end click
+  setGalleryInfo();
 
-  $('#gallery-thumbs a:first').click();
-}); // end ready
+  images.forEach(function(image) {
+    image.addEventListener("click", function() {
+      swapImage(image, displayImage);
+      setAsCurrent(image);
+      setGalleryInfo();
+    });
+  });
+});
+
+// $(document).ready(function() {
+
+//   $('#gallery-thumbs a').click(function(event) {
+//     //don't follow link
+//     event.preventDefault();
+//     //get path to new image
+//     var imgPath = $(this).attr('href');
+//     //get reference to old image
+//     var oldImage = $('#gallery-photo img');
+
+//     //create HTML for new image
+//     var newImage = $('<img src="' + imgPath +'">');
+//     //make new image invisible
+//     newImage.hide();
+//     //add to the #photo div
+//     $('#gallery-photo').prepend(newImage);
+//     //fade in new image
+//     var fade_time = 1000;
+
+//     $('#gallery-info h3').html($(this).find('.photo-title').html() || 'No Title');
+//     $('#gallery-info p').html($(this).find('.photo-desc').html() || 'No Description');
+
+//     if(!$(this).hasClass('current')) {
+//       $('.current').removeClass('current')
+//       $(this).addClass('current');
+//     }
+
+//      //fade out old image and remove from DOM
+//      oldImage.fadeOut(fade_time, function(){
+//        $(this).remove();
+//       });
+
+//      newImage.fadeIn(fade_time);
+//   }); // end click
+
+//   $('#gallery-thumbs a:first').click();
+// }); // end ready
 
 
   // $('#gallery img').each(function(i) {
